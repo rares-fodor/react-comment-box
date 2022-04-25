bc80c904d148e1fbd88c088d699e5aa4017bef5d
Rares Fodor <rares.fodor@stud.acs.upb.ro>
2022-04-25 22:48:20 +0300

    indents >:(

--- src/index.js
+++ src/index.js
@@ -5,29 +5,29 @@ import './index.css';
 const MAX_COMMENTS_DISPLAYED = 10;
 
 class CommentBox extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-		};
-	}
+        };
+    }
 
-	getComments() {}
-	newComment() {}
+    getComments() {}
+    newComment() {}
 
 
-	render() {
-		return (
-			// TODO: one div for each reply
-			<div className="CommentBox">
-			<div className="Reply">
-			</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            // TODO: one div for each reply
+            <div className="CommentBox">
+            <div className="Reply">
+            </div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
-	<CommentBox/>,
-	document.getElementById('root'),
+    <CommentBox/>,
+    document.getElementById('root'),
 );
